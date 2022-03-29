d214094deb7d4b81724eb3a7e5641ad373836067
igomez-p <ire.go.pla@gmail.com>
2022-03-29 15:51:20 -0400

    checkamount

--- cpp0/ex02/Account.cpp
+++ cpp0/ex02/Account.cpp
@@ -6,21 +6,22 @@
 /*   By: igomez-p <igomez-p@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 14:54:03 by kali              #+#    #+#             */
-/*   Updated: 2022/03/28 18:04:33 by igomez-p         ###   ########.fr       */
+/*   Updated: 2022/03/29 15:07:11 by igomez-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "account.hpp"
+#include <iostream>
+#include "Account.hpp"
 
-static int	Account::getNbAccounts( void )   { return (this->_nbAccounts); }
+int	Account::getNbAccounts( void )   { return (t._nbAccounts); }
 
-static int	Account::getTotalAmount( void )  { return (this->_totalAmount); }
+int	Account::getTotalAmount( void )  { return (t._totalAmount); }
 
-static int	Account::getNbDeposits( void )   { return (this->_totalNbDeposits); }
+int	Account::getNbDeposits( void )   { return (t._totalNbDeposits); }
 
-static int	Account::getNbWithdrawals( void ) { return (this->_totalNbWithdrawals); }
+int	Account::getNbWithdrawals( void ) { return (t._totalNbWithdrawals); }
 
-static void	Account::displayAccountsInfos( void )
+void	Account::displayAccountsInfos( void )
 {
     std::cout << "accounts:" << getNbAccounts() << ";";
     std::cout << "total:" << getTotalAmount() << ";";
@@ -36,22 +37,30 @@ Account::~Account( void )
     this->_amount = 0;
     this->_nbDeposits = 0;
     this->_nbWithdrawals = 0;
+
+    this->_nbAccounts = 0;
+    this->_totalAmount = 0;
+    this->_totalNbDeposits = 0;
+    this->_totalNbWithdrawals = 0;
 }
 
-void	Account::makeDeposit( int deposit )  { this->_nbDeposits = deposit; }
+void	Account::makeDeposit( int deposit )  { this->_nbDeposits += deposit; }
 bool	Account::makeWithdrawal( int withdrawal )
 {
     this->_nbWithdrawals = withdrawal;
     return (this->_nbWithdrawals);
 }
 
-int		Account::checkAmount( void ) const { return (this->_amount == getTotalAmount()); }
+int		Account::checkAmount( void ) const
+{
+    return (this->_amount == (int)(this->getTotalAmount() - this->getNbWithdrawals()));
+}
 
 void	Account::displayStatus( void ) const
 {
     // TODO
 
-    std::cout << "index:" << this->_accountIndex << ";";
+  /*  std::cout << "index:" << this->_accountIndex << ";";
     std::cout << "amount:" << this->_amount << ";";
     // "p_amount" ??
     if ()
@@ -62,5 +71,5 @@ void	Account::displayStatus( void ) const
         std::cout << "withdrawals:" << this->_nbWithdrawals << std::endl;
     }
     else if ()
-        std::cout << "withdrawals:refused\n";
+        std::cout << "withdrawals:refused\n";*/
 }
